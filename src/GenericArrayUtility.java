8fb0e768f3022d22f8841a7fac1b9ae2ec5fce8b
Sibilla-Heureuse <sibillaheureuse@gmail.com>
2026-02-17 23:38:23 +0200

    Generic array Utility

--- src/GenericArrayUtility.java
+++ src/GenericArrayUtility.java
@@ -15,10 +15,10 @@ public class GenericArrayUtility {
             return Arrays.copyOf(a, a.length);
         }
 
-        // Create new array with combined length
+
         T[] merged = Arrays.copyOf(a, a.length + b.length);
 
-        // Copy elements of b into merged array
+
         System.arraycopy(b, 0, merged, a.length, b.length);
 
         return merged;
